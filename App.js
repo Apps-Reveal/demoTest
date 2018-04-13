efc68f97356a9442341c5da17a1f1c06c9a2be32
johnnynode <johnnynode@gmail.com>
2018-04-13 12:31:33 +0800

    remove index.android.js and index.ios.js add index.js

--- App.js
+++ App.js
@@ -6,13 +6,13 @@
 
 import React, {Component} from 'react';
 import {
-    AppRegistry,
     StyleSheet,
     Text,
     View
 } from 'react-native';
 
-export default class App extends Component {
+type Props = {};
+export default class App extends Component<Props> {
     render() {
         return (
             <View style={styles.container}>
@@ -66,5 +66,3 @@ const styles = StyleSheet.create({
         padding: 30,
     }
 });
-
-AppRegistry.registerComponent('App', () => App);
